06bff5acaacf48905af2c1f0aed2fded025648e5
AMITABH MUKHERJEE <amitabh@vectraglobal.com>
2024-07-23 15:33:08 +0530

    Added Comments

--- Wrapper/Entity.h
+++ Wrapper/Entity.h
@@ -8,9 +8,13 @@ namespace CLI
     public ref class Entity : public ManagedObject<Core::Entity>
     {
     public:
+        //declare all the members from the core class that you want to access from the .NET context.
 
+        // Constructor
         Entity(String^ name, float xPos, float yPos);
         void Move(float deltaX, float deltaY);
+
+        //Create Properties to access these methods from C#
         property float XPosition
         {
         public:
